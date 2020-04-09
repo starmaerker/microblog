1d1eb35edcc9a23f86eacfd84d457c6beb81d153
Andreas Gropp <starmaerker@googlemail.com>
2020-04-09 21:45:37 +0200

    add config.py

--- app/routes.py
+++ app/routes.py
@@ -1,11 +1,20 @@
 from flask import render_template
 from app import app
-import time
+import datetime
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    time_act = time.time()
+    time_act = datetime.datetime.now()
 
-    return render_template('index.html', title='Online-Quizsystem', time=time_act)
+    questions = [
+        {
+            'What is love?': 'I dont know.',
+            'Who is it?': 'Michael Jackson',
+            'Where is the gold?': 'Under the Rainbow',
+            'How is the weather?': 'Its sunny.'
+        }
+    ]
+
+    return render_template('index.html', questions=questions, time=time_act)
