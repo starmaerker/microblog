05490e197ca9828a1d765731c3f0942a094e2358
Andreas Gropp <starmaerker@googlemail.com>
2020-04-14 14:40:43 +0200

    add user registration

--- app/forms.py
+++ app/forms.py
@@ -13,8 +13,8 @@ class LoginForm(FlaskForm):
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
-    email = StringField('Email', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
 
