4259d9aa5080538a0d7ccf193a3c9f2d0ecf3cad
Andreas Gropp <starmaerker@googlemail.com>
2020-04-28 15:03:24 +0200

    add reset_password.html

--- config.py
+++ config.py
@@ -13,6 +13,6 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['starmaerker@googlemail.com']
+    ADMINS = ['andreas.gropp@outlook.com']
 
     POSTS_PER_PAGE = 3
